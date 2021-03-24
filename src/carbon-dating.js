31d8b07bf6976b6788e2543c8acf108435b74f92
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-25 00:28:44 +0300

    feat: carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,20 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  
+  if(isNaN(sampleActivity) || typeof(sampleActivity) != 'string'){
+    return false;
+  }
+  
+  if(sampleActivity < 15 && sampleActivity > 0){
+    let sample = parseFloat(sampleActivity);
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let answer = (Math.log(MODERN_ACTIVITY / sample)) / k;
+    return Math.ceil(answer);
+  } else {
+    return false;
+  }
 };
