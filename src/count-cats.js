3f5bfe141c870da3780f4ea34979b7b38ea88a94
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-24 23:04:55 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+// module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let cats = 0;
+  for(x=0; x < matrix.length; x++){
+    for(j=0; j < matrix[x].length; j++){
+      if(matrix[x][j] == "^^"){
+        cats++;
+      }
+    }
+  }
+  return cats;
 };
