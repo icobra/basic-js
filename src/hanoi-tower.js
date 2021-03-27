e02dc2a7f47c11e0eeb0772c34cb39899742df36
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-27 16:22:52 +0300

    feat: add hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let answer = {
+    turns: 0,
+    seconds: 0
+  }
+
+  answer.turns = Math.pow(2, disksNumber) - 1;
+  const HOUR = 3600;
+  answer.seconds =  Math.floor((answer.turns * HOUR) / turnsSpeed);
+
+  return answer;
 };
