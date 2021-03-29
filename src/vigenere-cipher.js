7ec3403e2d92cfb756e6077b9c76fe9426f046be
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-29 20:49:31 +0300

    feat has error

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.direct = true;
+    if (direct === false) {
+      this.direct = false;
+    }
+  }
+
+  new_key(text, key){
+    let k = Math.floor(text.lenght / key.lenght) + 1;
+    let new_key;
+    while(k > 0){
+      new_key += key;
+      k--;
+    }
+    new_key = new_key.toUpperCase()
+
+    return new_key;
+  }
+  
+  encrypt(text,key) {
+    if(typeof text !== 'string' || typeof key !== 'string'){
+      throw Error("There aren't parameters");
+    }
+    let code_text = text.toUpperCase();
+    let answer = "";
+    for(let x = 0; x < code_text.lenght; x++){
+      if(/[A-Z]/.test(code_text[x])){
+        symbol = String.fromCharCode((code_text[x].charCodeAt() + new_key[x].charCodeAt()) % 26 + "A".charCodeAt());
+        answer += symbol;
+      } else {
+        answer += text[x];
+      }
+    }
+    if (!this.direct) answer.split("").reverse().join("");
+    return answer
+  }
+
+  decrypt(text, key) {
+    if(typeof text !== 'string' || typeof key !== 'string'){
+      throw Error("There aren't parameters");
+    }
+    let new_key = this.new_key(text, key);
+    let answer = "";
+    let symbol = "";
+    for(let x = 0; x < code_text.lenght; x++){
+      if(/[A-Z]/.test(code_text[x])){
+        symbol = String.fromCharCode((code_text[x].charCodeAt() - new_key[x].charCodeAt()) % 26 + "A".charCodeAt());
+        answer += symbol;
+      } else {
+        answer += text[x];
+      }
+    }
+    if (!this.direct) answer.split("").reverse().join("");
+    return answer;
   }
 }
 
