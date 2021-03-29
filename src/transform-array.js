a355742b2a015e461d3c9861d77cec3e2ec263a5
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-29 21:03:25 +0300

    feat has error

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+   let answer = [];
+   for(x=0; x < arr.length; x++){
+    if(arr[x] == '--discard-next'){
+       x++;
+    }else if(arr[x] == '--discard-prev'){
+      answer.pop();
+    }else if(arr[x] == '--double-next'){
+      answer.push(arr[x++]);     
+    }else if(arr[x] == '--double-prev'){
+      answer.push(answer[-1]);
+    }else{
+      answer.push(arr[x]);
+    }
+   }
+  return answer;
 };
