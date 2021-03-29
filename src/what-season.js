aab3d122fa41a6ed2c47b8136c6989cfb603f10d
Sasha Good aka icobra <eiroglif@mail.ru>
2021-03-29 18:50:44 +0300

    feat: add what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (date == null) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (!(date instanceof Date && !isNaN(date.valueOf()))) {
+    throw new Error("Input is not a date");
+  }
+
+  let month = date.getMonth();
+
+  if(month == 0 || month == 1 || month == 11){
+    return 'winter';
+  }
+  if(month == 2 || month == 3 || month == 4){
+    return 'spring';
+  }
+  if(month == 5 || month == 6 || month == 7){
+    return 'summer';
+  }
+  if(month == 8 || month == 9 || month == 10){
+    return 'autumn';
+  }
 };
